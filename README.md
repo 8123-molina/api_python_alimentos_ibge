896858ea1e89ce73ecb87169c12f0f69260c1a7b
Dhyone Molina <molinaprojetos@gmail.com>
2023-08-24 14:58:49 -0300

    conectando a banco de dados sql e ajustando rotas e dados

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
 
-# 1 - Objetivo -> Criar uma API de consulta de alimentos na IBGE e TBCA
-# 2 - URL base ->  localhost
-# 3 - Endpoints -> 
-#     - localhost/alimentos (GET)
-#     - localhost/alimentos (POST)
-#     - localhost/alimentos/id (GET)
-#     - localhost/alimentos/id (PUT)
-#     - localhost/alimentos/id (DELETE)
-# 4 - Quais recursos -> alimentos
+1 - Objetivo -> Criar uma API de consulta de alimentos na IBGE e TBCA
+2 - URL base ->  localhost
+3 - Endpoints -> 
+     - localhost/alimentos (GET) - retorno todos os alimentos contidos na tabela ibge_aliementos
+     - localhost/alimentos (POST) - 
+     - localhost/alimentos/id (GET)
+     - localhost/alimentos/id (PUT)
+     - localhost/alimentos/id (DELETE)
+4 - Quais recursos -> alimentos
--- app.py
+++ app.py
@@ -1,63 +1,125 @@
-
+import mysql.connector
 from flask import Flask, jsonify, request
 
+mydb = mysql.connector.connect(
+    host='localhost', 
+    user='root', 
+    password='',
+    database='db_alimentos'
+)
 app = Flask(__name__)
+my_cursor = mydb.cursor()
 
-livros = [
-    {
-        "id": 1,
-        'titulo': 'O senhor dos aneis',
-        'autor': 'J.K Howling 1'
-    },
-    {
-        "id": 2,
-        'titulo': 'O senhor dos aneis o retorno do rei',
-        'autor': 'J.K Howling 2'
-    },
-    {
-        "id": 3,
-        'titulo': 'O senhor dos aneis as duas torres',
-        'autor': 'J.K Howling 3'
-    }
-]
+# Consultar por descrição alimento
+@app.route('/alimentos/pesquisar/<string:text>', methods=['GET'])
+def pesquisar_alimentos(text):
+    sql = f"SELECT * FROM ibge_alimentos where descricao LIKE ('%{text}%')"
+    my_cursor.execute(sql)
+    res_alimentos = my_cursor.fetchall()
 
-# Consultar todos
-@app.route('/livros', methods=['GET'])
-def obter_livros():
-    return jsonify(livros)
+    alimentos = list()
+    for alimento in res_alimentos:
+        print(alimento[3])
+        alimentos.append(
+            {
+                'id': alimento[0],
+	            'cd_ibge': alimento[1],
+                'descricao': alimento[2],
+	            'cd_md_preparo': alimento[3],
+	            'energia_kcal': alimento[4],
+	            'proteina_g': alimento[5],
+	            'lipidios_totais_g': alimento[6],
+	            'carboidrato_g': alimento[7],
+	            'fibra_alimentar_total_g': alimento[8],
+	            'calcio_mg': alimento[9],
+	            'magnesio_mg': alimento[10],
+	            'manganes_mg': alimento[11],
+	            'foforo_mg': alimento[12],
+	            'ferro_mg': alimento[13],
+	            'sodio_mg': alimento[14],
+	            'sodio_adicao_mg': alimento[15],
+	            'potassio_mg': alimento[16],
+	            'cobre_mg': alimento[17],
+	            'zinco_mg': alimento[18],
+	            'selenio_mcg': alimento[19],
+	            'colesterol_mg': alimento[20],
+	            'ag_saturados_g': alimento[21],
+	            'ag_mono_g': alimento[22],
+	            'ag_poli_g': alimento[23],
+	            'ag_linoleico_g': alimento[24],
+	            'ag_linolenico_g': alimento[25],
+	            'ag_transtotal_g': alimento[26],
+	            'acucar_total_g': alimento[27],
+	            'acucar_adicao_g': alimento[28],
+	            'retinol_mcg': alimento[29],
+	            'vitamina_a_rae_mcg': alimento[30],
+	            'tiamina_mg': alimento[31],
+	            'riboflavina_mg': alimento[32],
+	            'niacina_mg': alimento[33],
+	            'niacina_ne_mg': alimento[34],
+	            'piridoxina_mg': alimento[35],
+	            'cabolamina_mcg': alimento[36],
+	            'folato_dfe_mcg': alimento[37],
+	            'vitamina_d_mg': alimento[38],
+	            'vitamina_e_mg': alimento[39],
+	            'vitamina_c_mg': alimento[40],
+	            'created_at': alimento[41],
+	            'updated_at': alimento[42],
+            }
+        )
+    itens = len(alimentos)
+    return jsonify(
+        mensagem='Sua pesquisa retornou '+str(itens)+' para o texto: ('+text+')',
+        dados=alimentos
 
-# Consultar (id)
-@app.route('/livros/<int:id>', methods=['GET'])
-def obter_livro_to_id(id):
-    for livro in livros:
-        if livro.get('id') == id:
-            return jsonify(livro)
+    )
 
-# Editar livro por (id)
-@app.route('/livros/<int:id>', methods=['PUT'])
-def editar_livro_to_id(id):
-    livro_alterado = request.get_json()
-    for indice,livro in enumerate(livros):
-        if livro.get('id') == id:
-            livros[indice].update(livro_alterado)
-            return jsonify( "Livro encontrado:",livros[indice], "Será alterado para :", livros[indice])
 
-# Incluir novo livro
-@app.route('/livros', methods=['POST'])
-def incluir_novo_livro():
-    novo_livro = request.get_json()
-    livros.append(novo_livro)
+# Consultar alimeto(id)
+@app.route('/alimentos/<int:id>', methods=['GET'])
+def obter_alimento_to_id(id):
+    sql = f"SELECT * FROM ibge_alimentos where descricao LIKE ('%{id}%')"
+    my_cursor.execute(sql)
+    res_alimentos = my_cursor.fetchall()
 
-    return jsonify(livros)
+    return jsonify(res_alimentos)
 
-# Excluir livro (id)
-@app.route('/livros/<int:id>', methods=['DELETE'])
-def excluir_livro(id):
-    for indice,livro in enumerate(livros):
-        if livro.get('id') == id:
-            del livros[indice]
 
-    return jsonify(livros)
+# Consultar modo preparo (id)
+@app.route('/alimentos/<int:id>', methods=['GET'])
+def obter_alimento_to_id(id):
+    sql = f"SELECT * FROM ibge_alimentos where descricao LIKE ('%{id}%')"
+    my_cursor.execute(sql)
+    res_alimentos = my_cursor.fetchall()
+
+    return jsonify(res_alimentos)
+
+
+# Consultar modo de preparo alimento por (id)
+@app.route('/alimentos/modo-preparo/<int:id>', methods=['PUT'])
+def editar_alimento_to_id(id):
+    alimento_alterado = request.get_json()
+    for indice,alimento in enumerate(alimentos):
+        if alimento.get('id') == id:
+            alimentos[indice].update(alimento_alterado)
+            return jsonify( "alimento encontrado:",alimentos[indice], "Será alterado para :", alimentos[indice])
+
+# Incluir novo alimento
+@app.route('/alimentos', methods=['POST'])
+def incluir_novo_alimento():
+    novo_alimento = request.get_json()
+    alimentos.append(novo_alimento)
+
+    return jsonify(alimentos)
+
+# Excluir alimento (id)
+@app.route('/alimentos/<int:id>', methods=['DELETE'])
+def excluir_alimento(id):
+    for indice,alimento in enumerate(alimentos):
+        if alimento.get('id') == id:
+            del alimentos[indice]
+
+    return jsonify(alimentos)
 
 
 app.run(port=5000,host='localhost',debug=True)
